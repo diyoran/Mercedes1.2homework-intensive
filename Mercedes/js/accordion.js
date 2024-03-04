3cd89ae10c405a5798b1cbe24f1fa68a07ab7acf
Diyora <157114574+diyoran@users.noreply.github.com>
2024-03-04 17:57:01 +0500

    интенсив

---
+++ Mercedes/js/accordion.js
@@ -0,0 +1,46 @@
+const lists = document.querySelectorAll('.feature-sub');
+const btns = document.querySelectorAll('.feature__link');
+
+const openTab = (tab, idx) => {
+    tab.classList.add('feature__link_active')
+    lists[idx].classList.remove('hidden')
+}
+
+const closeTab = (tab, idx) => {
+    tab.classList.remove('feature__link_active')
+    lists[idx].classList.add('hidden')
+
+}
+
+btns.forEach((btnItem, index) => {
+    btnItem.addEventListener('click', () => {
+
+        if (btnItem.classList.contains(`feature__link_active`)) {
+            closeTab(btnItem, index)
+        } else {
+            openTab(btnItem, index)
+        }
+
+
+
+
+
+        btns.forEach((btnItem, idx) => {
+            if (index !== idx) {
+                btnItem.classList.remove('feature__link_active')
+            }
+
+        })
+
+        lists.forEach((listItem, idx) => {
+            if (index !== idx) {
+                listItem.classList.add('hidden')
+            }
+          
+        })
+
+
+    })
+})
+
+
